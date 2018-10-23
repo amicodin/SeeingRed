9962c31b1e4286247a59cf7e2b46e8b60fc401e0
Anthony G Micodin <amicodin@o244-20.csuchico.edu>
2018-10-22 17:02:51 -0700

    finished

--- main.cpp
+++ main.cpp
@@ -8,6 +8,42 @@ using namespace std;
 //argv - each argument word, stored in an array
 int main(int argc, char* argv[])
 {
-  
+  string filename;
+  Bitmap image;
+        
+  if ( argc != 2)
+  {
+    cout<<"Please specify one image file!\n";
+  }        
+
+  else
+  {
+    filename = argv[1];
+    image.open(filename);
+    if ( image.isImage() )
+      {
+        vector <vector <Pixel> > imagePixels = image.toPixelMatrix();
+
+for ( int i=0; i<imagePixels.size(); i++)
+{
+  for (int j=0; j<imagePixels[1].size(); j++)
+  {
+    Pixel color =imagePixels[i][j];
+    color.green =0;
+    color.blue = 0;
+    imagePixels[i][j] = color;
+  }
+}
+   image.fromPixelMatrix( imagePixels) ;
+   image.save("redness.bmp");
+
+
+        cout<<filename<<" is "<<imagePixels.size()<<" pixels high and "<<imagePixels[1].size()<<" pixels wide"<<endl;
+      }   
+    else 
+    {
+      cout<<"Image file must be a bitmap with 24-bit colo depth\n";
+    }        
+  }        
   return 0;
 }
